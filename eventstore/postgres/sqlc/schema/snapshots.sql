27bb82212630ae6eb558fd54862d40ee6653a161
gingfrederik <jeff26912317@gmail.com>
2024-02-27 00:16:05 +0800

    fix: snapshots table pk

--- eventstore/postgres/sqlc/schema/snapshots.sql
+++ eventstore/postgres/sqlc/schema/snapshots.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS "snapshots" (
-    "aggregate_id" UUID PRIMARY KEY,
+    "aggregate_id" UUID NOT NULL,
     "aggregate_type" VARCHAR(2048) NOT NULL,
     "version" INTEGER NOT NULL,
     "data" BYTEA NOT NULL,
-    "timestamp" TIMESTAMP NOT NULL
+    "timestamp" TIMESTAMP NOT NULL,
+    PRIMARY KEY("aggregate_id", "version")
 );
